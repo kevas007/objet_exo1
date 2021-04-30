2c73b977497be87c60d1494c797b5cca32997040
kevAs <kevassiobo@gmail.com>
2021-04-30 17:11:17 +0200

    base

---
+++ main.js
@@ -0,0 +1,23 @@
+//exo1
+let personnage ={
+    nom : "san",
+    prenom:"goku",
+    age: 25 + " ans",
+    taille: 180 + "cm"
+} ;
+console.log(`Votre age est ${personnage.age}`);
+//exo2
+let personnage2 ={
+    nom : "san",
+    prenom:"goanh",
+    age: 15 +"ans",
+    taille: 175 + "cm"
+};
+let personnage3 ={
+
+};
+//exo3
+    personnage3.nom = personnage.nom;
+    personnage3.age = personnage2.age,
+    personnage3.taille = 170 +"cm",
+console.log(personnage3);
